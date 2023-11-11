d0c9c9e36e887a285938e864a98e1dc3139562d7
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-11-11 19:23:42 -0300

    add useful links for Rust macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,3 @@
-/*
-
-https://github.com/napi-rs/napi-rs/blob/529317b5efe01e49137e8c42c6e90ca351805cce/crates/backend/src/typegen.rs#L9
-
-https://stackoverflow.com/questions/64020061/is-there-a-way-of-removing-quotation-marks-when-using-the-quote-crate
-
-https://www.reddit.com/r/rust/comments/ibbw4v/using_the_quote_crate_how_to_use_the_quote_macro/
-https://www.reddit.com/r/rust/comments/ibbw4v/comment/g1uy8ok/
-*/
 use quote::quote;
 use syn::{Data, DeriveInput, Field, Fields, parse_macro_input, Type};
 
---
+++ useful-links-for-Rust-macros.md
@@ -0,0 +1,7 @@
+https://github.com/napi-rs/napi-rs/blob/529317b5efe01e49137e8c42c6e90ca351805cce/crates/backend/src/typegen.rs#L9
+
+https://stackoverflow.com/questions/64020061/is-there-a-way-of-removing-quotation-marks-when-using-the-quote-crate
+
+https://www.reddit.com/r/rust/comments/ibbw4v/using_the_quote_crate_how_to_use_the_quote_macro/
+
+https://www.reddit.com/r/rust/comments/ibbw4v/comment/g1uy8ok/
