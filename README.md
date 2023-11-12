78a1771c4e8de1b22c6cfb940fa0acdc05b8e793
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-11-12 11:38:00 -0300

    fix: now it should handle every case

--- README.md
+++ README.md
@@ -22,8 +22,6 @@ impl Cube {
 }
 ```
 
-It only works for basic types for now, see issues.
-
 ps: Rust Analyzer already generates `new` function for you!
 
 ![Generate new](imgs/rust-analyzer-new-function.png)
@@ -43,15 +41,62 @@ use new_derive::New;
 
 #[derive(New)]
 struct Cube {
-	r: i32,
-	c: i32,
-	z: i32,
-	w: i32,
+    r: i32,
+    c: i32,
+    z: i32,
+    w: i32,
+}
+
+#[derive(Debug, New)]
+struct V {
+	b: Vec<String>,
+}
+
+#[derive(Debug, New)]
+struct B {
+	b: Box<String>
+}
+
+#[derive(Debug, New)]
+struct S1 {
+	array1: [i32; 3],
+	name: String,
+}
+
+#[derive(Debug, New)]
+struct S2 {
+	opt1: Option<String>,
+	idx: usize,
 }
 
 fn main() {
-	let cube = Cube::new(1, 2, 3, 4);
-	println!("{}", cube.r * cube.c * cube.z * cube.w);
+    let cube = Cube::new(1, 2, 3, 4);
+    println!("{}", cube.r * cube.c * cube.z * cube.w);
+
+	let s1 = S1::new([1, 2, 3], "Leandro".into());
+	dbg!(s1);
+
+	let s2 = S2::new(Some("ola".into()), 30);
+	dbg!(s2);
 }
 ```
 
+Output:
+
+```
+24
+[src/main.rs:38] s1 = S1 {
+    array1: [
+        1,
+        2,
+        3,
+    ],
+    name: "Leandro",
+}
+[src/main.rs:41] s2 = S2 {
+    opt1: Some(
+        "ola",
+    ),
+    idx: 30,
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,103 +1,62 @@
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Field, Fields, Type};
 
-fn get_name_type(f: &Field) -> (String, String) {
-    let param_name = f.ident.as_ref().unwrap().to_string();
-    match &f.ty {
-        Type::Path(syn::TypePath { qself: None, path }) => {
-            for p in &path.segments {
-                println!("{}", p.ident.to_string());
-                match &p.arguments {
-                    syn::PathArguments::None => {
-						println!("No args. Nothing to worry about?! =)");
-					}
-                    syn::PathArguments::AngleBracketed(args) => {
-                        for arg in &args.args {
-                            dbg!(arg);
-                            match arg {
-                                syn::GenericArgument::Lifetime(_) => todo!(),
-                                syn::GenericArgument::Type(t) => {
-                                    // println!("generic arg type is {}", t.);
-                                    todo!("handle generic argument");
-                                }
-                                syn::GenericArgument::Const(_) => {
-									println!("TODO handle Const?");
-								}
-                                syn::GenericArgument::AssocType(_) => todo!(),
-                                syn::GenericArgument::AssocConst(_) => todo!(),
-                                syn::GenericArgument::Constraint(_) => todo!(),
-                                _ => {
-									println!("TODO handle arg _");
-								}
-                            }
-                        }
-                    }
-                    syn::PathArguments::Parenthesized(_) => todo!(),
+struct Struct {
+    name: String,
+    parameters: String,
+}
+
+impl Struct {
+    fn parse(input: String) -> Self {
+        let tmp: Vec<&str> = input.split_ascii_whitespace().collect();
+        let name = tmp[1].into();
+        let parameters = Struct::get_inner(&input);
+        
+        Self {
+            name,
+            parameters,
+        }
+    }
+
+    fn get_inner(input: &str) -> String {
+        let l = input.find('{').unwrap() + 1;
+        let r = {
+            let mut r = 0;
+            for i in (0..input.len()).rev() {
+                if &input[i..i+1] == "}" {
+                    r = i;
+                    break;
                 }
             }
-            if let Some(syn::PathSegment { ident, arguments }) = path.segments.last() {
-                // let (a, b, c) = arguments
-                return (param_name, ident.to_string());
+            r
+        };
+        input[l..r].into()
+    }
+
+    fn get_fields(&self) -> String {
+        let mut ret = String::new();
+        let tmp: Vec<&str> = self.parameters.split(':').collect();
+        if tmp.is_empty() { return "".into(); }
+        ret.push_str(tmp[0]);
+        for i in 1..tmp.len() - 1 {
+            ret.push_str(", ");
+            if tmp[i].contains(',') {
+                let tmp2: Vec<&str> = tmp[i].split(',').collect();
+                ret.push_str(tmp2.last().unwrap());
+            } else {
+                ret.push_str(tmp[i]);
             }
         }
-        Type::Path(syn::TypePath { qself, path }) => {
-			println!("qself is some");
-        }
-		Type::Array(_) => {
-			todo!("TODO handle Array");
-		}
-        _ => todo!("which type?!"),
-    }
-    todo!()
-}
 
-fn get_parameters(parameters: &[(String, String)]) -> String {
-    let mut ret = String::new();
-    for p in parameters {
-        if !ret.is_empty() {
-            ret.push_str(", ");
-        }
-        ret.push_str(&mut p.0.clone());
-        ret.push_str(": ");
-        ret.push_str(&mut p.1.clone());
+        ret
     }
-    ret
-}
-
-fn get_fields(parameters: &[(String, String)]) -> String {
-    let mut ret = String::new();
-    for p in parameters {
-        if !ret.is_empty() {
-            ret.push_str(", ");
-        }
-        ret.push_str(&mut p.0.clone());
-    }
-    ret
 }
 
 #[proc_macro_derive(New)]
 pub fn derive_new(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    // Parse the input tokens into a syntax tree.
-    let input = parse_macro_input!(input as DeriveInput);
-
-    // Used in the quasi-quotation below as `#name`.
-    let name = input.ident;
-    let mut parameters: Vec<(String, String)> = vec![];
-    match input.data {
-        Data::Struct(ref data) => match data.fields {
-            Fields::Named(ref fields) => {
-                for f in &fields.named {
-                    parameters.push(get_name_type(f));
-                }
-            }
-            _ => todo!(),
-        },
-        _ => todo!(),
-    }
-	dbg!(&parameters);
-
-    let params: proc_macro2::TokenStream = get_parameters(&parameters).parse().unwrap();
-    let fields: proc_macro2::TokenStream = get_fields(&parameters).parse().unwrap();
+    let s = Struct::parse(input.to_string());
+    let name: proc_macro2::TokenStream = s.name.parse().unwrap();
+    let params: proc_macro2::TokenStream = s.parameters.parse().unwrap();
+    let fields: proc_macro2::TokenStream = s.get_fields().parse().unwrap();
 
     let expanded = quote! {
         // The generated impl.
